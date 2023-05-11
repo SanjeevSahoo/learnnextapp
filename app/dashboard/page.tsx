a257576b7292c3e71c6fbba8921f8c56c78ee723
Sanjeev Sahoo <sanjeevs.ttl@tatamotors.com>
2023-05-11 17:05:58 +0530

    loadding ui

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,16 @@
+const delayFetch = () =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve({ data: 1 }), 5000);
+  });
 
-function Dashboard() {
-  return <div>Dashboard Page</div>;
+const loadDataFromServer = async () => {
+  const response = await delayFetch();
+  return response;
+};
+
+async function Dashboard() {
+  const articles = await loadDataFromServer();
+  return <div>Dashboard Page {JSON.stringify(articles)}</div>;
 }
 
 export default Dashboard;
